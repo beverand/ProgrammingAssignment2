183d7a03ac464de8ab20652d9a3aded28b86130d
beverand <beverly.andrews@gmail.com>
2016-08-06 23:32:45 -0700

    final assignment2 updated names

--- assignment2EXAMPLE.R
+++ assignment2EXAMPLE.R
@@ -11,3 +11,36 @@ plusFunctions <- function() {
  
  list(plustwo =plustwo, plusthree = plusthree)
 }
+
+## Vector object that has 4 accessor functions
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+
+## this function verifies that the mean value exists and returns it
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
--- cachematrix.R
+++ cachematrix.R
@@ -1,66 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Vector object that has 4 accessor functions
-
-  makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-  }
-
-
-## this function verifies that the mean value exists and returns it
-
-  cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-  }
-  
-  
   #this function creates a special matrix object with 4 accessor
   #methods to manipulate internal variables
   makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    i <- NULL
     set <- function(y) {
       x <<- y
-      m <<- NULL
+      i <<- NULL
     }
     get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
+    setinverse <- function(solve) i <<- solve
+    getinverse <- function() i
     list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+         setinverse = setinverse,
+         getinverse = getinverse)
   }
   
   
   ## this function returns a matrix that is the inverse of x 
   ## input: makeCacheMatrix output: inverse of makeCacheMatrix
   cacheSolve <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
+    i <- x$getinverse()
+    if(!is.null(i)) {
       message("getting cached data")
-      return(m)
+      return(i)
     }
     data <- x$get()
-    m <- solve(data, ...)
-    x$setmean(m)
-    m
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
   }
   
